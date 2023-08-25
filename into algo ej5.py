6643e629a4abd1d7ff1c7cdb786898ac90eab793
lautaro2427 <142542859+lautaro2427@users.noreply.github.com>
2023-08-25 15:25:59 -0300

    Update into algo ej5.py

--- into algo ej5.py
+++ into algo ej5.py
@@ -1,11 +1,12 @@
-cp=(input("ingrese la cantidad de pagina"))
+cp=int(input("ingrese la cantidad de pagina"))
+
 precio=500+cp*20.20
 
 if cp >600:
     precio+=536
     
 else:
-    if cap>300:
+    if cp>300:
         precio+=200
 print("el precio a pagar es",precio)        
         
