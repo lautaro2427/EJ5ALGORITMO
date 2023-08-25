1c1be6fccbe6a998cfadba0902058bf2bc002543
lautaro2427 <142542859+lautaro2427@users.noreply.github.com>
2023-08-25 15:21:07 -0300

    Add files via upload

---
+++ into algo ej5.py
@@ -0,0 +1,11 @@
+cp=(input("ingrese la cantidad de pagina"))
+precio=500+cp*20.20
+
+if cp >600:
+    precio+=536
+    
+else:
+    if cap>300:
+        precio+=200
+print("el precio a pagar es",precio)        
+        
